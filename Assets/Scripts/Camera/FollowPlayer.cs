0b4bc45b814b3c1459cbf0b5f6647848174eb03a
Darkyon0831 <adrian.rondahl@live.se>
2023-03-27 14:23:03 +0200

    Added Follow Player camera script

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: SerializeField]
+    public GameObject Player { get; set; }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Player.transform.position.x - transform.position.x, Player.transform.position.y - transform.position.y, 0.0f);
+
+        int i = 0;
     }
 }
