08f8891a80dfb226bce2959da2b9cd660f0602cf
Darkyon0831 <adrian.rondahl@live.se>
2023-03-27 20:02:02 +0200

    Added Wall Collision script

---
+++ Assets/Scripts/Player/WallCollision.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallCollision : MonoBehaviour
+{
+    private float sizeY = 0.0f;
+    private float sizeX = 0.0f;
+    private BoxCollider2D boxCollider = null;
+
+    private void Start()
+    {
+        sizeY = transform.localScale.y / 2.0f;
+        sizeX = transform.localScale.x / 2.0f;
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void checkHit(Vector3 pos, Vector3 dir, float distance)
+    {
+        LayerMask mask = LayerMask.GetMask("Wall");
+        RaycastHit2D hit = Physics2D.Raycast(pos, dir, distance, mask);
+
+        if (hit.collider != null)
+        {
+            ColliderDistance2D distance2D = hit.collider.Distance(boxCollider);
+            transform.position += dir * distance2D.distance;
+        }
+    }
+
+    private void Update()
+    {
+
+
+        LayerMask mask = LayerMask.GetMask("Wall");
+
+        // Up and down rays
+        checkHit(transform.position + Vector3.left * sizeX, Vector3.up, sizeY);
+        checkHit(transform.position + Vector3.right * sizeX, Vector3.up, sizeY);
+        checkHit(transform.position + Vector3.left * sizeX, Vector3.down, sizeY);
+        checkHit(transform.position + Vector3.right * sizeX, Vector3.down, sizeY);
+
+        // Left and right rays
+        checkHit(transform.position + Vector3.up * sizeY, Vector3.left, sizeX);
+        checkHit(transform.position + Vector3.down * sizeY, Vector3.left, sizeX);
+        checkHit(transform.position + Vector3.up * sizeY, Vector3.right, sizeX);
+        checkHit(transform.position + Vector3.down * sizeY, Vector3.right, sizeX);
+    }
+}
