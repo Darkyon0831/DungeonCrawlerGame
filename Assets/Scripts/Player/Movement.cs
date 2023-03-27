d4083be70c15dccf050876eb8906c82fa58c6f5e
Darkyon0831 <adrian.rondahl@live.se>
2023-03-27 13:56:27 +0200

    Update Movement script

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,27 +8,55 @@ public class Movement : MonoBehaviour
     [field: SerializeField]
     public float MovementSpeed { get; set; }
 
-    // Start is called before the first frame update
-    void Start()
+    [field: SerializeField]
+    public KeyCode UpKey { get; set; }
+
+    [field: SerializeField]
+    public KeyCode DownKey { get; set; }
+
+    [field: SerializeField]
+    public KeyCode LeftKey { get; set; }
+
+    [field: SerializeField]
+    public KeyCode RightKey { get; set; }
+
+
+    class KeyboardInput
     {
-        
+        float ud = 0;
+        float lr = 0;
+
+        public float UD { get { return ud; } set { if (value != ud) { Debug.Log("Input value UD changed, from " + ud + " to " + value); } ud = value; } }
+        public float LR { get { return lr; } set { if (value != lr) { Debug.Log("Input value LR changed, from " + lr + " to " + value); } lr = value; } }
     }
 
-    int KeyToValue(string key)
+    KeyboardInput keyboardInput = new KeyboardInput();
+
+    void UpdateInput()
     {
-        if (Input.GetButtonDown(key)) 
-            return 1;
-        else
-            return 0;
+        float udCopy = keyboardInput.UD;
+        float lrCopy = keyboardInput.LR;
+
+        if (Input.GetKeyUp(UpKey)) udCopy -= 1.0f;
+        if (Input.GetKeyUp(DownKey)) udCopy += 1.0f;
+        if (Input.GetKeyUp(LeftKey)) lrCopy += 1.0f;
+        if (Input.GetKeyUp(RightKey)) lrCopy -= 1.0f;
+
+
+        if (Input.GetKeyDown(UpKey)) udCopy += 1.0f;
+        if (Input.GetKeyDown(DownKey)) udCopy -= 1.0f;
+        if (Input.GetKeyDown(LeftKey)) lrCopy -= 1.0f;
+        if (Input.GetKeyDown(RightKey)) lrCopy += 1.0f;
+
+        keyboardInput.UD = udCopy;
+        keyboardInput.LR = lrCopy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(
-            (KeyToValue("w") + -KeyToValue("s")) * MovementSpeed,
-            (KeyToValue("d") + -KeyToValue("a")) * MovementSpeed,
-            0.0f
-            );
+        UpdateInput();
+
+        transform.Translate(keyboardInput.LR * MovementSpeed, keyboardInput.UD * MovementSpeed, 0.0f);
     }
 }
