aac83503d66bbf994cd53bbd4e89690cff96e750
Darkyon0831 <adrian.rondahl@live.se>
2023-03-28 15:01:13 +0200

    Update Follow Player

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FollowPlayer : MonoBehaviour
 {
     [field: SerializeField]
     public GameObject Player { get; set; }
 
+    private Vector3 position = new Vector3(0, 0, 0);
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Player.transform.position.x - transform.position.x, Player.transform.position.y - transform.position.y, 0.0f);
-
-        int i = 0;
+        position.Set(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+        transform.position = position;
     }
 }
