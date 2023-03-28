268a2f4386733a8feccc0f76cd45bc33fd053922
Darkyon0831 <adrian.rondahl@live.se>
2023-03-28 15:00:26 +0200

    Added Mirror

---
+++ Assets/Scripts/Player/MirrorCollision.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MirrorCollision : MonoBehaviour
+{
+    private Vector3 oldPosition = Vector3.zero;
+    private Vector3 dir = Vector3.zero;
+
+    [field: SerializeField]
+    public float RayOffset { get; set; }
+
+    [field: SerializeField]
+    public float HitDistance { get; set; } = 0;
+
+    private float sizeY = 0.0f;
+    private float sizeX = 0.0f;
+
+    [field: SerializeField]
+    public bool IsMirrorHit { get; set; }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPosition = transform.position;
+        sizeY = transform.localScale.y / 2.0f;
+        sizeX = transform.localScale.x / 2.0f;
+    }
+
+    private void CheckCollison(Vector3 pos, Vector3 dir, float distance)
+    {
+        if (IsMirrorHit) return;
+
+        Collision2DHit hit = Collision2D.CheckHit(pos, dir, distance, "Mirror");
+
+        Debug.DrawRay(pos, dir * distance, Color.blue);
+
+        if (hit != null)
+            IsMirrorHit = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        IsMirrorHit = false;
+
+        // Up and down rays
+        CheckCollison(transform.position + Vector3.left * (sizeX - RayOffset), Vector3.up, sizeY + HitDistance);
+        CheckCollison(transform.position + Vector3.right * (sizeX - RayOffset), Vector3.up, sizeY + HitDistance);
+        CheckCollison(transform.position + Vector3.left * (sizeX - RayOffset), Vector3.down, sizeY + HitDistance);
+        CheckCollison(transform.position + Vector3.right * (sizeX - RayOffset), Vector3.down, sizeY + HitDistance);
+
+        // Left and right rays
+        CheckCollison(transform.position + Vector3.up * (sizeY - RayOffset), Vector3.left, sizeX + HitDistance);
+        CheckCollison(transform.position + Vector3.down * (sizeY - RayOffset), Vector3.left, sizeX + HitDistance);
+        CheckCollison(transform.position + Vector3.up * (sizeY - RayOffset), Vector3.right, sizeX + HitDistance);
+        CheckCollison(transform.position + Vector3.down * (sizeY - RayOffset), Vector3.right, sizeX + HitDistance);
+
+        oldPosition = transform.position;
+    }
+}
